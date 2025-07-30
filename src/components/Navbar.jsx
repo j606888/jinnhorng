7a0de228e35e22199805a2586cf524e48baadc96
James <j606888@gmail.com>
2025-07-30 18:49:39 +0800

    Add link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -99,17 +99,19 @@ const Navbar = ({ white = false }) => {
           : "bg-transparent"
       )}
     >
-      <div className="flex items-center gap-2.5">
-        <Logo color={white && !isScolled ? "#fff" : "#DA3947"} />
-        <h1
-          className={clsx(
-            "text-lg font-medium tracking-[3.24px]",
-            white && !isScolled ? "text-white" : "text-[#30241E]"
-          )}
-        >
-          晉弘實業股份有限公司
-        </h1>
-      </div>
+      <Link href="/">
+        <div className="flex items-center gap-2.5">
+          <Logo color={white && !isScolled ? "#fff" : "#DA3947"} />
+          <h1
+            className={clsx(
+              "text-lg font-medium tracking-[3.24px]",
+              white && !isScolled ? "text-white" : "text-[#30241E]"
+            )}
+          >
+            晉弘實業股份有限公司
+          </h1>
+        </div>
+      </Link>
       <Sidebar white={white && !isScolled} />
       <div className="hidden xl:flex gap-16 items-center">
         {LINKS.slice(0, 5).map((link) =>
